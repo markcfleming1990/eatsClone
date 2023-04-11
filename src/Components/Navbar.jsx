1b910698f81bd1850dfcce849010d797930949a2
markcfleming <markcfleming1990@gmail.com>
2023-04-11 13:39:10 -0400

    fixed typo error

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,11 +15,12 @@ const Navbar = () => {
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       {/* Left side */}
+      {/* This bottom div holds the app name as well as a toggle button */}
       <div className="flex items-center">
         <div onClick={() => setNav(!nav)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
-        <h1 className="text-2xl sm:tex-3xl lg:text-4xl px-2">
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
           Best <span className="font-bold">Eats</span>
         </h1>
         <div
